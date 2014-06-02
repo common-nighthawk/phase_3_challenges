20b6734ba07af6d4d89c322ca1adf9dc385ff6a2
Anne Baer & Daniel Deutsch <pair+albaer.common-nighthawk@devbootcamp.com>
2014-06-02 16:56:36 -0500

    all crud functionality for category model

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,42 @@
+class CategoriesController < ApplicationController
+  def index
+    @categories = Category.all
+  end
+
+  def show
+    @category = Category.find(params[:id])
+    # @listings = @category.listings
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    @category.update_attributes(name: params[:category][:name])
+    redirect_to @category
+  end
+
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+
+    redirect_to categories_path
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(name: params[:category][:name])
+    if @category.save
+      redirect_to @category
+    else
+      render 'new'
+    end
+  end
+
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+categories = [{name: 'Jobs'},
+              {name: 'Furniture'},
+              {name: 'Missed Connections'},
+              {name: 'Bikes'}]
+
+Category.create(categories)
